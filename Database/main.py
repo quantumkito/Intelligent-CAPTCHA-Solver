185017f064ad04e2ad880514f54435f9fec0213e
quantumkito <mayakumari20082016@gmail.com>
2025-05-13 22:30:16 +0530

    Commit

--- Database/main.py
+++ Database/main.py
@@ -1,6 +1,15 @@
-from dotenv import load_dotenv, find_dotenv
+from dotenv import load_dotenv
 import os
 import pprint
 from pymongo import MongoClient
+import certifi
 
+load_dotenv()
+mongo_password = os.getenv("MONGO_PASSWORD")
+
+client = MongoClient(f"mongodb+srv://username:{mongo_password}@cluster.mongodb.net/?retryWrites=true&w=majority", tlsCAFile=certifi.where())
+
+dbs = client.list_database_names()
+BreakingCaptcha_db = client.BreakingCaptcha
+collections = BreakingCaptcha_db.list_collection_names()
 
