a748ff9fef6fd661b7869f37d509894bce64b7aa
quantumkito <mayakumari20082016@gmail.com>
2025-05-15 14:40:52 +0530

    Updating train.py

--- AI/DigitalRecognition/train.py
+++ AI/DigitalRecognition/train.py
@@ -8,3 +8,25 @@ from tensorflow.keras.layers import Dense
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from sklearn.model_selection import KFold
 import numpy
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train = x_train.reshape(-1, 28, 28, 1)
+x_test = x_test.reshape(-1, 28, 28, 1)
+x_train = x_train / 255.0
+x_test = x_test / 255.0
+y_train = to_categorical(y_train, num_classes=10)
+y_test = to_categorical(y_test, num_classes=10)
+
+train_datagen = ImageDataGenerator(
+    rescale = 1./255,
+    rotation_range = 10,
+    zoom_range = 0.2,
+    width_shift_range = 0.2,
+    height_shift_range = 0.2,
+    shear_range = 0.15,
+    fill_mode = 'nearest',
+    horizontal_flip = True
+)
+
+test_datagen = ImageDataGenerator()
