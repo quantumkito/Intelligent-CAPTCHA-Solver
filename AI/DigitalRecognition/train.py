69df08302981991cc1b15d11360fc52eb481445e
quantumkito <mayakumari20082016@gmail.com>
2025-05-15 14:19:46 +0530

    Adding train.py

---
+++ AI/DigitalRecognition/train.py
@@ -0,0 +1,10 @@
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from sklearn.model_selection import KFold
+import numpy
