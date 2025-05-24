700608deaa98a1041fa6917a07dee2d060993ffb
Kito <mayakumari20082016@gmail.com>
2025-05-24 23:55:55 +0530

    Updating VG16.py

--- AI/ImageCAPTCHA/Object/Code/VG16.py
+++ AI/ImageCAPTCHA/Object/Code/VG16.py
@@ -3,7 +3,11 @@ import pandas as pd
 import os
 import matplotlib.pyplot as plt
 import cv2
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import InputLayer, Flatten, Dropout, Dense
+from sklearn.model_selection import train_test_split
 import tensorflow as tf
+from tensorflow.keras.applications.vgg16 import VGG16
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 tf.random.set_seed(42)
@@ -41,3 +45,44 @@ datagen = ImageDataGenerator(
     fill_mode='nearest',
     rotation_range=4
 )
+
+X = []
+for img_path in df['img']:
+    img = cv2.imread(str(img_path))
+    img = cv2.resize(img, (128, 128))
+    img = img / 255.0  
+    X.append(img)
+
+y = df['encode_label']
+
+X_train, X_test_val, y_train, y_test_val = train_test_split(X, y, random_state=42)
+X_test, X_val, y_test, y_val = train_test_split(X_test_val, y_test_val, random_state=42)
+
+it_train = datagen.flow(np.array(X_train), y_train.to_numpy(), batch_size=32)
+it_val = datagen.flow(np.array(X_val), y_val.to_numpy(), batch_size=32)
+
+base_model = VGG16(input_shape=(128, 128, 3), include_top=False, weights='imagenet')
+
+for layer in base_model.layers:
+    layer.trainable = False
+
+model = Sequential([
+    InputLayer(input_shape=(128, 128, 3)),
+    base_model,
+    Flatten(),
+    Dropout(0.3),  
+    Dense(256, activation='relu'),
+    Dense(256, activation='relu'),
+    Dropout(0.3),  
+    Dense(len(df_labels), activation='softmax') 
+])
+
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(learning_rate=0.00002),
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+print(model.summary())
+
+history = model.fit(it_train, epochs=20, validation_data=it_val)
